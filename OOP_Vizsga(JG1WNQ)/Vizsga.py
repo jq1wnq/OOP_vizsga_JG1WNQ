c328c1bd55bf5ab1aa87e64d45a75a5f884e1062
takacs.gabor <jg1wnq@neptun.gde.hu>
2024-01-16 19:29:21 +0100

    4

--- OOP_Vizsga(JG1WNQ)/Vizsga.py
+++ OOP_Vizsga(JG1WNQ)/Vizsga.py
@@ -1,6 +1,6 @@
 class Utanfuto:
     def __init__(self, tipus, ar, teherbiras):
-        self.tipus = Utanfutotipus.tipus
+        self.tipus = tipus
         self.ar = ar
         self.teherbiras = teherbiras
 
@@ -14,35 +14,52 @@ class Kolcsonzo:
         self.kor = kor
 
 class Kolcsonzes:
-    def __init__(self, kolcsonzonev, honap, nap, tipus, ar):
+    def __init__(self, kolcsonzonev, honap, nap, ktipus, ar):
         self.kolcsonzonev = kolcsonzonev
         self.honap = honap
         self.nap = nap
-        self.tipus = tipus
+        self.ktipus = ktipus
         self.ar = ar
+
+    def __str__(self):
+        return f"{self.kolcsonzonev}, {self.honap}, {self.nap}, {self.ktipus}, {self.ar} "
+
+    def getkolcsonzonev(self):
+        for kolcsonzonev in self.kolcsonzonev:
+            print(f"{self.kolcsonzonev}, {self.honap}, {self.nap}, {self.ktipus}, {self.ar}")
 
 
 
 
 kolcs1 = Kolcsonzes("Ági", "Január", 8, "Ponyvás", "10 000 Ft")
 kolcs2 = Kolcsonzes("János", "Február", 27, "Ponyvás", "10 000 Ft")
+foglalasok = [kolcs1, kolcs2]
 
-foglalasok= [kolcs1, kolcs2]
-
-Utanfutotipus.utipus = ["Fekezett", "Ponyvas"]
-
-
-kolcsi = Kolcsonzes(input("Add meg az ügyfél nevét: "),
-input("Add meg a foglalás hónapját: "),
-input("Add meg a foglalás napját: "),
-input("Add meg a típust(Fekezett, Ponyvas): "),
-input("Add meg az árat: "))
+afoglalas = Kolcsonzes
+print(afoglalas)
 
 
 
 
 
-print(foglalasok)
+for i in range(2):
+    ugyfel = input("Add meg az ügyfél nevét: ")
+    fhonap = input("Add meg a foglalás hónapját: ")
+    fnap = input("Add meg a foglalás napját: ")
+    ftipus = input("Add meg a típust(Fekezett, Ponyvas): ")
+    far = input("Add meg az árat: ")
+    print(ugyfel, fhonap, fnap, ftipus, far)
+
+
+
+
+
+
+
+print(kolcs1)
+
+
+
 
 
 
